b75d50ccc4122551e8c9909e1f923b07562758b8
Aaron Whitehead <whitehead.aaron@live.com>
2018-08-27 09:04:14 -0700

    finished second challenge

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,9 +1,9 @@
 'use strict';
 
-/* global store, cuid */
+/* global store, Item */
 
 // eslint-disable-next-line no-unused-vars
-const shoppingList = (function(){
+const shoppingList = (function () {
 
   function generateItemElement(item) {
     let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
@@ -14,7 +14,7 @@ const shoppingList = (function(){
         </form>
       `;
     }
-  
+
     return `
       <li class="js-item-element" data-item-id="${item.id}">
         ${itemTitle}
@@ -28,39 +28,46 @@ const shoppingList = (function(){
         </div>
       </li>`;
   }
-  
-  
+
+
   function generateShoppingItemsString(shoppingList) {
     const items = shoppingList.map((item) => generateItemElement(item));
     return items.join('');
   }
-  
-  
+
+
   function render() {
     // Filter item list if store prop is true by item.checked === false
     let items = store.items;
     if (store.hideCheckedItems) {
       items = store.items.filter(item => !item.checked);
     }
-  
+
     // Filter item list if store prop `searchTerm` is not empty
     if (store.searchTerm) {
       items = store.items.filter(item => item.name.includes(store.searchTerm));
     }
-  
+
     // render the shopping list in the DOM
     console.log('`render` ran');
     const shoppingListItemsString = generateShoppingItemsString(items);
-  
+
     // insert that HTML into the DOM
     $('.js-shopping-list').html(shoppingListItemsString);
   }
-  
-  
+
+
   function addItemToShoppingList(itemName) {
-    store.items.push({ id: cuid(), name: itemName, checked: false });
+    try {
+      Item.validateName(itemName);
+      store.items.push(Item.create(itemName));
+    }
+    catch (error) {
+      console.log(`Cannot add item: ${error.message}`)
+    }
+    render();
   }
-  
+
   function handleNewItemSubmit() {
     $('#js-shopping-list-form').submit(function (event) {
       event.preventDefault();
@@ -70,19 +77,19 @@ const shoppingList = (function(){
       render();
     });
   }
-  
+
   function toggleCheckedForListItem(id) {
     const foundItem = store.items.find(item => item.id === id);
     foundItem.checked = !foundItem.checked;
   }
-  
-  
+
+
   function getItemIdFromElement(item) {
     return $(item)
       .closest('.js-item-element')
       .data('item-id');
   }
-  
+
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
@@ -90,26 +97,26 @@ const shoppingList = (function(){
       render();
     });
   }
-  
+
   function deleteListItem(id) {
     const index = store.items.findIndex(item => item.id === id);
     store.items.splice(index, 1);
   }
-  
+
   function editListItemName(id, itemName) {
     const item = store.items.find(item => item.id === id);
     item.name = itemName;
   }
-  
+
   function toggleCheckedItemsFilter() {
     store.hideCheckedItems = !store.hideCheckedItems;
   }
-  
+
   function setSearchTerm(val) {
     store.searchTerm = val;
   }
-  
-  
+
+
   function handleDeleteItemClicked() {
     // like in `handleItemCheckClicked`, we use event delegation
     $('.js-shopping-list').on('click', '.js-item-delete', event => {
@@ -121,7 +128,7 @@ const shoppingList = (function(){
       render();
     });
   }
-  
+
   function handleEditShoppingItemSubmit() {
     $('.js-shopping-list').on('submit', '.js-edit-item', event => {
       event.preventDefault();
@@ -131,14 +138,14 @@ const shoppingList = (function(){
       render();
     });
   }
-  
+
   function handleToggleFilterClick() {
     $('.js-filter-checked').click(() => {
       toggleCheckedItemsFilter();
       render();
     });
   }
-  
+
   function handleShoppingListSearch() {
     $('.js-shopping-list-search-entry').on('keyup', event => {
       const val = $(event.currentTarget).val();
@@ -146,7 +153,7 @@ const shoppingList = (function(){
       render();
     });
   }
-  
+
   function bindEventListeners() {
     handleNewItemSubmit();
     handleItemCheckClicked();
